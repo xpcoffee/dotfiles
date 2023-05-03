fc9c4b8671ccc027dda5bcbafdd78a97df414b96
Emerick Bosch <xpc.dev@gmail.com>
2023-05-03 11:52:36 +0200

    Commit latest changes

--- fish/config.fish
+++ fish/config.fish
@@ -4,13 +4,6 @@
 function fish_greeting
 end
 
-# tmux
-# if type -q tmux
-#     if test "$TERM" != "screen" -a "$TERM" != "tmux" -a -z "$TMUX"
-#         status is-interactive; and tmux
-#     end
-# end
-
 # === fzf ===
 set FZF_CTRL_T_COMMAND "ag -l"
 
@@ -37,6 +30,12 @@ function ws --description 'Change directory to a repo'
    cd "$WS_DIR/$argv[1]"
 end
 complete -f -c ws -n "not __fish_seen_subcommand_from (repos)" -a "(repos)"
+
+function vimws --description 'Open workspace in vim'
+   cd "$WS_DIR/$argv[1]"
+   vim
+end
+complete -f -c vimws -n "not __fish_seen_subcommand_from (repos)" -a "(repos)"
 
 function current_repo --description 'Returns the current repo'
     if string match -rq "$WS_DIR.+" -- (pwd)
