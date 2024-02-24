28a64d004349b7d2fbf4a1dee7f4c439e90b3e5f
Emerick Bosch <xpc.dev@gmail.com>
2024-02-24 12:22:50 +0000

    Fix install

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ set -euo pipefail
 ####################################
 
 declare -A osInfo;
-osInfo[/etc/debian_version]="apt-get install -y"
-osInfo[/etc/alpine-release]="apk --update add"
-osInfo[/etc/centos-release]="yum install -y"
-osInfo[/etc/fedora-release]="dnf install -y"
+osInfo[/etc/debian_version]="apt-get update && apt-get install -y stow"
+osInfo[/etc/alpine-release]="apk update && apk --update add stow"
+osInfo[/etc/centos-release]="yum update && yum install -y stow"
+osInfo[/etc/fedora-release]="dnf check-update && dnf install -y stow"
 
 for f in ${!osInfo[@]}
 do
@@ -28,17 +28,17 @@ done
 if ! command -v stow &> /dev/null
 then
     echo "GNU stow not found. Installing it using ${package_manager_install}."
-    ${package_manager_install} stow
+    ${package_manager_install}
 fi
 
 #################
 #     $HOME     #
 #################
 
-HOME_DIRECTORY_CONFIG=("vim", "tmux", "zsh", "emac", "git")
+HOME_DIRECTORY_CONFIG=("vim" "tmux" "zsh" "emacs" "git")
 
 for config in ${HOME_DIRECTORY_CONFIG[*]}; do
-    stow "${config}" --target="${HOME}"
+   stow --adopt --target="${HOME}" "${config}"
 done
 
 ################
@@ -51,5 +51,9 @@ HOME_CONFIG_DIR="${HOME}/.config"
 mkdir -p "${HOME_CONFIG_DIR}"
 
 for config in ${CONFIG_DIRECTORY_CONFIG[*]}; do
-    stow "${config}" --target=${HOME_CONFIG_DIR}
+    stow --adopt --target=${HOME_CONFIG_DIR} "${config}" 
 done
+
+echo "Dotfiles installed."
+echo "Differences:"
+git status -s
