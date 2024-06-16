b0e557b7358506fd18f8dfa42d42234e25985f26
Emerick Bosch <xpc.dev@gmail.com>
2024-06-16 11:21:34 +0200

    Completion bindings

--- nvim/lua/xpcoffee/plugins/nvim-cmp.lua
+++ nvim/lua/xpcoffee/plugins/nvim-cmp.lua
@@ -2,7 +2,7 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-        "hrsh7th/cmp-buffer", -- completion source of current buffer
+        "hrsh7th/cmp-buffer",   -- completion source of current buffer
         "hrsh7th/cmp-path",
         "onsails/lspkind.nvim", -- vscode-like pictograms in completion
     },
@@ -14,6 +14,14 @@ return {
             completion = {
                 completeopt = "menu,menuone,preview,noselect",
             },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+                ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+            }),
             sources = cmp.config.sources({
                 --ordering matters for priority
                 { name = "nvim_lsp" },
