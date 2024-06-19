3379e3273d6c1146a3f8f6e47d0fc6157f7ae2a1
Emerick Bosch <xpc.dev@gmail.com>
2024-06-19 21:46:14 +0200

    Get commenting to work as I expect

--- nvim/lua/xpcoffee/plugins/comment.lua
+++ nvim/lua/xpcoffee/plugins/comment.lua
@@ -11,5 +11,19 @@ return {
         comment.setup({
             pre_hook = ts_context_commentstring.create_pre_hook(),
         })
+
+        local api = require("Comment.api");
+        vim.keymap.set({ "n" }, "<C-_>", api.toggle.linewise.current, {})
+
+        -- get region comments to work
+        -- https://github.com/numToStr/Comment.nvim/blob/master/lua/Comment/api.lua#L94
+        local esc = vim.api.nvim_replace_termcodes(
+            '<ESC>', true, false, true
+        )
+        local toggleSelection = function()
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            api.toggle.linewise(vim.fn.visualmode())
+        end
+        vim.keymap.set({ "v" }, "<C-_>", toggleSelection, {})
     end,
 }
