77b54f78c61f6370cb617f1cbae63fdeeda96f87
Emerick Bosch <xpc.dev@gmail.com>
2024-07-12 00:24:11 +0200

    Show a status in toolbar when recording
    Useful because recording mode can affect/pause autocompletion results.

--- nvim/lua/xpcoffee/plugins/lualine.lua
+++ nvim/lua/xpcoffee/plugins/lualine.lua
@@ -1,8 +1,23 @@
+local function isRecording()
+    local reg = vim.fn.reg_recording()
+    if reg == "" then return "" end -- not recording
+    return "recording to " .. reg
+end
+
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         local lualine = require("lualine")
-        lualine.setup({})
+        lualine.setup {
+            sections = {
+                lualine_y = {
+                    {
+                        isRecording,
+                        color = { bg = '#8EC07C', fg = '#0A0A0A', gui = 'none' },
+                    }
+                }
+            }
+        }
     end
 }
