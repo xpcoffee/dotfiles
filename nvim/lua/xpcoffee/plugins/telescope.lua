ff0a9a676db2968adbddcec8aaa77b60ae6073ac
Emerick Bosch <xpc.dev@gmail.com>
2024-07-21 09:07:32 +0200

    Live grep selection

--- nvim/lua/xpcoffee/plugins/telescope.lua
+++ nvim/lua/xpcoffee/plugins/telescope.lua
@@ -18,11 +18,23 @@ return {
 
         telescope.load_extension("fzf")
 
-        -- keymaps
         local builtins = require('telescope.builtin')
+        local function live_grep_selection()
+            local selection = vim.fn.getregion(
+                vim.fn.getpos("v"),
+                vim.fn.getpos(".")
+            )
+
+            builtins.live_grep {
+                default_text = table.concat(selection)
+            }
+        end
+
+        -- keymaps
         require("which-key").add({ "<leader>f", group = "files" })
         vim.keymap.set('n', '<leader>ff', builtins.find_files, { desc = 'Find file' })
         vim.keymap.set('n', '<leader>fg', builtins.live_grep, { desc = 'Grep through files' })
+        vim.keymap.set('v', '<leader>fg', live_grep_selection, { desc = 'Grep through files' })
         vim.keymap.set('n', '<leader>fb', builtins.buffers, { desc = 'Find buffer' })
     end
 }
