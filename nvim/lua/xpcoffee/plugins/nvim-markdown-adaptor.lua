c540174d40248f0fb58f75b4e163dec0eb71ab6c
Emerick Bosch <xpc.dev@gmail.com>
2024-08-08 23:22:15 +0200

    Add keybinds for working with custom plugin

--- nvim/lua/xpcoffee/plugins/nvim-markdown-adaptor.lua
+++ nvim/lua/xpcoffee/plugins/nvim-markdown-adaptor.lua
@@ -1,4 +1,9 @@
 return {
-    "xpcoffee/nvim-markdown-adaptor",
-    dev = true
+  "xpcoffee/nvim-markdown-adaptor",
+  dev = true,
+  config = function()
+    local adaptor = require('nvim-markdown-adaptor')
+    vim.keymap.set("n", "<leader><leader>p", adaptor.convertFile)
+    vim.keymap.set("n", "<leader><leader>r", ':Lazy reload nvim-markdown-adaptor<cr>')
+  end
 }
