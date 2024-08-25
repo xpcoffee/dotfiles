69e5a6ebeae0dea10840cd8520cfbf9fd261c0b1
Emerick Bosch <xpc.dev@gmail.com>
2024-08-25 10:09:16 +0200

    Remap the macro key
    Many plugins use q to close windows and I end up recording macros, which
    gets in the way of autocompletion.

--- nvim/lua/xpcoffee/core/keymaps.lua
+++ nvim/lua/xpcoffee/core/keymaps.lua
@@ -7,27 +7,31 @@ keymap.set("n", "<leader>v", ":e ~/.config/nvim/init.lua<CR>", { desc = "Open ro
 keymap.set("n", "<leader>bn", "<cmd>bn<CR>", { desc = "Next buffer" })
 keymap.set("n", "<leader>bp", "<cmd>bp<CR>", { desc = "Previous buffer" })
 
+-- remap the macro key
+keymap.set("n", "<leader>q", "q", { desc = "Macro key" })
+keymap.set("n", "q", "<Nop>", {})
+
 -- quickfix list delete keymap
 function Remove_qf_item()
-    local curqfidx = vim.fn.line('.')
-    local qfall = vim.fn.getqflist()
+  local curqfidx = vim.fn.line('.')
+  local qfall = vim.fn.getqflist()
 
-    -- Return if there are no items to remove
-    if #qfall == 0 then return end
+  -- Return if there are no items to remove
+  if #qfall == 0 then return end
 
-    -- Remove the item from the quickfix list
-    table.remove(qfall, curqfidx)
-    vim.fn.setqflist(qfall, 'r')
+  -- Remove the item from the quickfix list
+  table.remove(qfall, curqfidx)
+  vim.fn.setqflist(qfall, 'r')
 
-    -- Reopen quickfix window to refresh the list
-    vim.cmd('copen')
+  -- Reopen quickfix window to refresh the list
+  vim.cmd('copen')
 
-    -- If not at the end of the list, stay at the same index, otherwise, go one up.
-    local new_idx = curqfidx < #qfall and curqfidx or math.max(curqfidx - 1, 1)
+  -- If not at the end of the list, stay at the same index, otherwise, go one up.
+  local new_idx = curqfidx < #qfall and curqfidx or math.max(curqfidx - 1, 1)
 
-    -- Set the cursor position directly in the quickfix window
-    local winid = vim.fn.win_getid() -- Get the window ID of the quickfix window
-    vim.api.nvim_win_set_cursor(winid, { new_idx, 0 })
+  -- Set the cursor position directly in the quickfix window
+  local winid = vim.fn.win_getid()   -- Get the window ID of the quickfix window
+  vim.api.nvim_win_set_cursor(winid, { new_idx, 0 })
 end
 
 vim.cmd("command! RemoveQFItem lua Remove_qf_item()")
