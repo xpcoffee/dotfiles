f237c40bfd9ca25d5b3c63959e938a00edfeaee9
Emerick Bosch <xpc.dev@gmail.com>
2024-09-07 17:28:54 +0200

    Add oil.nvim for file management

---
+++ nvim/lua/xpcoffee/plugins/oil.lua
@@ -0,0 +1,28 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  config = function()
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      keymaps = {
+        ["<CR>"] = "actions.select",
+        ["<C-p>"] = "actions.preview",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+        ["<C-c>"] = "actions.close",
+        ["<C-r>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+      },
+      view_options = {
+        show_hidden = true
+      },
+      use_default_keymaps = false
+    }
+    local oil = require('oil')
+    oil.setup(opts)
+    vim.keymap.set("n", "-", oil.open_float, { desc = "Open parent directory" })
+  end
+}
