101663e8180bb3bbc0a1c77fe0050a1b030eac61
Emerick Bosch <xpc.dev@gmail.com>
2024-09-07 17:29:48 +0200

    Format files

--- nvim/lua/xpcoffee/plugins/lazygit.lua
+++ nvim/lua/xpcoffee/plugins/lazygit.lua
@@ -1,16 +1,16 @@
 return {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    requires = {
-        "nvim-lua/plenary.nvim",
-    },
-    keys = {
-        { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  requires = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader><leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
 }
