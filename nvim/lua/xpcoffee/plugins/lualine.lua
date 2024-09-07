93510eb68f003386c0de82a67caf1859ebea0ee4
Emerick Bosch <xpc.dev@gmail.com>
2024-09-07 17:29:48 +0200

    Format files

--- nvim/lua/xpcoffee/plugins/lualine.lua
+++ nvim/lua/xpcoffee/plugins/lualine.lua
@@ -1,23 +1,23 @@
 local function isRecording()
-    local reg = vim.fn.reg_recording()
-    if reg == "" then return "" end -- not recording
-    return "recording to " .. reg
+  local reg = vim.fn.reg_recording()
+  if reg == "" then return "" end   -- not recording
+  return "recording to " .. reg
 end
 
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local lualine = require("lualine")
-        lualine.setup {
-            sections = {
-                lualine_y = {
-                    {
-                        isRecording,
-                        color = { bg = '#8EC07C', fg = '#0A0A0A', gui = 'none' },
-                    }
-                }
-            }
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+    lualine.setup {
+      sections = {
+        lualine_y = {
+          {
+            isRecording,
+            color = { bg = '#8EC07C', fg = '#0A0A0A', gui = 'none' },
+          }
         }
-    end
+      }
+    }
+  end
 }
--- nvim/lua/xpcoffee/plugins/treesitter.lua
+++ nvim/lua/xpcoffee/plugins/treesitter.lua
@@ -1,51 +1,52 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",          -- update parsers,
-    dependencies = {
-        "windwp/nvim-ts-autotag", -- autoclose tags
-    },
-    config = function()
-        local treesitter = require("nvim-treesitter.configs")
-        local parsers = require("nvim-treesitter.parsers")
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",            -- update parsers,
+  dependencies = {
+    "windwp/nvim-ts-autotag",     -- autoclose tags
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+    local parsers = require("nvim-treesitter.parsers")
 
-        treesitter.setup({
-            highlight = { enable = true }, -- syntax
-            indent = { enable = true },
-            autotag = { enable = true },   -- requires plugin
-            -- languages we want to autoinstall
-            ensure_installed = {
-                "bash",
-                "css",
-                "dockerfile",
-                "gitignore",
-                "graphql",
-                "html",
-                "java",
-                "javascript",
-                "jq",
-                "json",
-                "kotlin",
-                "lua",
-                "markdown",
-                "rust",
-                "tsx",
-                "typescript",
-                "vim",
-                "yaml",
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<leader>s",
-                    node_incremental = "<leader>s",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                }
-            }
-        })
+    treesitter.setup({
+      highlight = { enable = true },       -- syntax
+      indent = { enable = true },
+      autotag = { enable = true },         -- requires plugin
+      -- languages we want to autoinstall
+      ensure_installed = {
+        "bash",
+        "css",
+        "dockerfile",
+        "gitignore",
+        "graphql",
+        "html",
+        "java",
+        "javascript",
+        "jq",
+        "json",
+        "kotlin",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "rust",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<leader>s",
+          node_incremental = "<leader>s",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        }
+      }
+    })
 
-        -- user markdowm highlighting for mdx
-        parsers.mdx = "markdown"
-    end
+    -- user markdowm highlighting for mdx
+    parsers.mdx = "markdown"
+  end
 }
