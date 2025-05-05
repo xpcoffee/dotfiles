fded7dfc6557bf5cc72c85785145db23937e1bdc
Emerick Bosch <xpc.dev@gmail.com>
2025-05-05 22:29:59 +0200

    Ensure marksman lsp is installed
    Autocompletion in markdown files

--- nvim/lua/xpcoffee/plugins/lsp/mason.lua
+++ nvim/lua/xpcoffee/plugins/lsp/mason.lua
@@ -1,32 +1,33 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({})
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "bashls",
-        "html",
-        "omnisharp",
-        "lua_ls",
-        "rust_analyzer",
-        "tailwindcss",
-        "ts_ls",
-      }
-    })
+		mason.setup({})
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"bashls",
+				"html",
+				"omnisharp",
+				"lua_ls",
+				"marksman",
+				"rust_analyzer",
+				"tailwindcss",
+				"ts_ls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "js-debug-adapter"
-      }
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"js-debug-adapter",
+			},
+		})
+	end,
 }
