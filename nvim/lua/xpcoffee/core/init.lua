a92af33eb2e091c3a7dcef196f25e50efb130500
Emerick Bosch <xpc.dev@gmail.com>
2025-08-03 17:06:36 +0200

    Shave 140ms off of start time by specifying the clipboard program
    https://www.reddit.com/r/neovim/comments/ab01n8/improve_neovim_startup_by_60ms_for_free_on_macos/

--- nvim/lua/xpcoffee/core/init.lua
+++ nvim/lua/xpcoffee/core/init.lua
@@ -1,3 +1,16 @@
 require("xpcoffee.core.options")
 require("xpcoffee.core.keymaps")
 require("xpcoffee.core.filetypes")
+
+-- If you have xclip installed
+vim.g.clipboard = {
+  name = 'xclip',
+  copy = {
+    ['+'] = 'xclip -selection clipboard',
+    ['*'] = 'xclip -selection primary',
+  },
+  paste = {
+    ['+'] = 'xclip -selection clipboard -o',
+    ['*'] = 'xclip -selection primary -o',
+  },
+}
