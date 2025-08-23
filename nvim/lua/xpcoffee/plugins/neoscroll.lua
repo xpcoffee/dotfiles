de385386455dedc005c91e9ad770b86a15ffb42f
Emerick Bosch <xpc.dev@gmail.com>
2025-08-23 20:57:17 +0200

    Fix c-e mapping sometimes being the neo-scroll one

--- nvim/lua/xpcoffee/plugins/neoscroll.lua
+++ nvim/lua/xpcoffee/plugins/neoscroll.lua
@@ -1,6 +1,9 @@
 return {
   "karb94/neoscroll.nvim",
   config = function()
-    require('neoscroll').setup({})
+    require('neoscroll').setup({
+      mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                  '<C-y>', 'zt', 'zz', 'zb'}
+    })
   end
 }
