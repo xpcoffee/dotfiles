0e10f1c008c2d965156609e077a07638a7f5370f
Emerick Bosch <xpc.dev@gmail.com>
2025-10-02 19:00:14 +0200

    Add vscode config and use flash instead of easymotion

--- nvim/lua/xpcoffee/plugins/easymotion.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "easymotion/vim-easymotion",
-    event = { "BufReadPre", "BufNewFile" },
-    init = function()
-        vim.keymap.set("n", "<space><space>f", "<Plug>(easymotion-prefix)")
-        vim.keymap.set({ "n", "v" }, "f", "<Plug>(easymotion-prefix)s")
-    end,
-}
