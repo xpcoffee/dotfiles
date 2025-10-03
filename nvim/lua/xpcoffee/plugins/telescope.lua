c8d7337c8c9bc3fbc87ea67dc1582bfad003a41b
Emerick Bosch <xpc.dev@gmail.com>
2025-10-03 10:40:02 +0200

    Change bindings to match other IDEs
    Makes it easier to transition between vscode, jetbrains, and vim

--- nvim/lua/xpcoffee/plugins/telescope.lua
+++ nvim/lua/xpcoffee/plugins/telescope.lua
@@ -56,14 +56,14 @@ return {
 
     -- keymaps
     require("which-key").add({ "<leader>f", group = "files" })
-    vim.keymap.set('n', '<leader>ff', builtins.find_files, { desc = 'Find file' })
-    vim.keymap.set('n', '<leader>fg', builtins.live_grep, { desc = 'Grep through files' })
-    vim.keymap.set('v', '<leader>fg', live_grep_selection, { desc = 'Grep through files' })
+    vim.keymap.set('n', '<C-p>', builtins.find_files, { desc = 'Find file' })
+    vim.keymap.set({ 'n', 'v' }, '<C-f>', builtins.live_grep, { desc = 'Grep through files' })
     vim.keymap.set('n', '<leader><leader>u', builtins.lsp_references, { desc = 'Usages/references' })
     vim.keymap.set('n', '<leader><leader>f', builtins.lsp_document_symbols, { desc = 'File symbols' })
     vim.keymap.set('n', '<leader><leader>p', builtins.lsp_dynamic_workspace_symbols, { desc = 'Project symbols' })
     vim.keymap.set('n', '<leader><leader>m', builtins.marks, { desc = 'Marks' })
-    vim.keymap.set({'n', 'i', 'v'}, '<C-e>', list_oldfiles_for_cwd, { desc = 'Recent files', noremap = true, silent = true })
+    vim.keymap.set({ 'n', 'i', 'v' }, '<C-e>', list_oldfiles_for_cwd,
+      { desc = 'Recent files', noremap = true, silent = true })
 
 
     -- delete buffers
