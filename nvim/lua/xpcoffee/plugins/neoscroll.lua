4508d6c64c7b4a25a50df6d12c4e9e8b38e39e3b
Emerick Bosch <xpc.dev@gmail.com>
2025-10-03 10:40:02 +0200

    Change bindings to match other IDEs
    Makes it easier to transition between vscode, jetbrains, and vim

--- nvim/lua/xpcoffee/plugins/neoscroll.lua
+++ nvim/lua/xpcoffee/plugins/neoscroll.lua
@@ -2,7 +2,7 @@ return {
   "karb94/neoscroll.nvim",
   config = function()
     require('neoscroll').setup({
-      mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+      mappings = {'<C-u>', '<C-d>', '<C-b>',
                   '<C-y>', 'zt', 'zz', 'zb'}
     })
   end
