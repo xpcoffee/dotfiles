2e67cb5798af02517f88e6a26801ab3b6c662d52
Emerick Bosch <xpc.dev@gmail.com>
2026-02-23 09:11:11 +0100

    Use clip.exe on wsl

--- nvim/lua/xpcoffee/core/options.lua
+++ nvim/lua/xpcoffee/core/options.lua
@@ -19,6 +19,36 @@ opt.smartcase = true -- don't ignore case if you have mixed case in search
 opt.backspace = "indent,eol,start"
 
 -- clipboard
+local function is_whitespace_only(lines)
+  for _, line in ipairs(lines) do
+    if line:match("%S") then
+      return false
+    end
+  end
+  return true
+end
+
+if vim.fn.system("uname -r"):lower():find("microsoft") then
+  local paste_cmd = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = function(lines, _)
+        if is_whitespace_only(lines) then return end
+        vim.fn.system("clip.exe", lines)
+      end,
+      ["*"] = function(lines, _)
+        if is_whitespace_only(lines) then return end
+        vim.fn.system("clip.exe", lines)
+      end,
+    },
+    paste = {
+      ["+"] = function() return vim.fn.systemlist(paste_cmd) end,
+      ["*"] = function() return vim.fn.systemlist(paste_cmd) end,
+    },
+    cache_enabled = 0,
+  }
+end
 opt.clipboard:append("unnamedplus") -- system clipboard as default register
 
 -- don't attempt to load perl things
