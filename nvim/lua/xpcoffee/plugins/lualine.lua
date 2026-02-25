a41f5fefbe28e9831644a7ac0e8572be7a52bbf4
Emerick Bosch <xpc.dev@gmail.com>
2026-02-25 14:13:29 +0100

    Round off lualine

--- nvim/lua/xpcoffee/plugins/lualine.lua
+++ nvim/lua/xpcoffee/plugins/lualine.lua
@@ -10,6 +10,10 @@ return {
   config = function()
     local lualine = require("lualine")
     lualine.setup {
+      options = {
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+      },
       sections = {
         lualine_y = {
           {
